18313bdd5548259d5f25e09d5a3236b449294378
DevGabriel20 <gabriel2003.ea@gmail.com>
2024-05-27 00:28:07 -0300

    Segue o excluir

---
+++ excluir.php
@@ -0,0 +1,30 @@
+<?php
+
+$id = $_POST['id'];
+$email = $_POST['email'];
+
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "bd_farmacia";
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$apagar = " DELETE FROM clientes WHERE id = '$id' AND email = '$email'";
+$delete_cliente = mysqli_query($conn,$apagar);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="3;http://localhost/FARMACIA_KFBG/login.html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirecionando...</title>
+</head>
+<body>
+    <h1>Login Excluído!</h1>
+</body>
+</html>
